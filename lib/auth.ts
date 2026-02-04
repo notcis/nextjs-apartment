12d0db91d91f921e3f88b000de64c7ab61968af1
notcis <notcis07@gmail.com>
2026-02-04 18:59:39 +0700

    better auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/prisma";
@@ -9,4 +10,24 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  // --- แก้ไขตรงนี้ ---
+  // ให้ใช้ค่าจาก .env ตรงๆ ถ้าไม่มีค่อยถอยไปหา Vercel หรือ Localhost
+  baseURL: process.env.BETTER_AUTH_URL || " http://localhost:3000",
+
+  secret: process.env.BETTER_AUTH_SECRET,
+
+  // --- เพิ่มความปลอดภัยและการจัดการ Cookie ---
+  advanced: {
+    // ถ้าไม่มี HTTPS (รันผ่าน IP) ให้ตั้งเป็น false เพื่อให้ Cookie ทำงานได้
+    // แต่ถ้ามี SSL แล้ว ให้ใช้ true
+    useSecureCookies:
+      process.env.NODE_ENV === "production" &&
+      process.env.BETTER_AUTH_URL?.startsWith("https"),
+  },
+
+  trustedOrigins: [
+    "http://localhost:3000",
+    process.env.BETTER_AUTH_URL,
+    process.env.NEXT_PUBLIC_APP_URL, // เพิ่ม URL ฝั่งหน้าบ้าน
+  ].filter(Boolean) as string[],
 });
