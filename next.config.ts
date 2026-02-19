34bb599286d536e119c33c67d589e0b63dbb0e92
notcis <notcis07@gmail.com>
2026-02-19 10:36:58 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "standalone",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  /* config options here */
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-} as any;
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
